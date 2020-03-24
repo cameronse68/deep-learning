c84e6dc0ab7f3aa18b77f6258dbd4280e8230d81
sarah cameron <sarahcameron@sarahs-mbp.myfiosgateway.com>
2020-03-24 16:18:35 -0400

    updated to add a batch function and updated lab1 notebook

--- learning/utils/convert.py
+++ learning/utils/convert.py
@@ -1,4 +1,8 @@
 import numpy as np
+import logging
+
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.NullHandler())
 
 
 def vectorize_string(s):
@@ -9,3 +13,23 @@ def vectorize_string(s):
         new.append(char2num[s[char]])
     new=np.array(new)
     return new
+
+def get_batch(vectorized_string,seq_length,batch_size):
+    n=vectorized_string.shape[0]-1
+    idx=np.random.choice(n-seq_length,batch_size)
+    inputs=[]
+    outputs=[]
+    for i in idx:
+        input_batch=np.array(vectorized_string[i:(i+seq_length)])
+        output_batch=np.array(vectorized_string[i+1:((i+seq_length)+1)])
+        inputs.append(input_batch)
+        outputs.append(output_batch)
+    x_batch = np.reshape(inputs, [batch_size, seq_length])
+    y_batch = np.reshape(outputs, [batch_size, seq_length])
+    try: 
+        correct=(batch_size,seq_length)
+        assert(x_batch.shape==correct)
+    except (AssertionError) as e:
+        logger.error("{}".format(type(e)))
+        raise
+    return x_batch, y_batch
