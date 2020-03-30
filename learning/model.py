656ac99d0640aa5cccc48d35e3bbb53978653789
sarah cameron <sarahcameron@sarahs-mbp.myfiosgateway.com>
2020-03-30 10:21:32 -0400

    added rnn and lstm functions in model.py

---
+++ learning/model.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+
+def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
+  model = tf.keras.Sequential([
+    tf.keras.layers.Embedding(vocab_size, embedding_dim, batch_input_shape=[batch_size, None]),
+    LSTM(rnn_units), 
+  ])
+  return model
+
+def LSTM(rnn_units): 
+  return tf.keras.layers.LSTM(
+    rnn_units, 
+    return_sequences=True, 
+    recurrent_initializer='glorot_uniform',
+    recurrent_activation='sigmoid',
+    stateful=True,
+  )
